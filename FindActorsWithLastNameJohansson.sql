6a087d0bc96ef59707b5ca752117a491bb555d8d
jcprice12 <john.c.price12@gmail.com>
2019-03-18 20:03:55 -0400

    last name johansson

---
+++ FindActorsWithLastNameJohansson.sql
@@ -0,0 +1,7 @@
+#Questions from https://datamastery.gitlab.io/exercises/sakila-queries.html
+# Which actors have the last name ‘Johansson’
+USE sakila;
+
+SELECT *
+FROM actor
+WHERE last_name = "Johansson";
